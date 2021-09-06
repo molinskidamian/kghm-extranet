81454d43e148b595623eb7241c7014831c1ce24f
Damian Molinski <tedeer@gmail.com>
2021-09-06 20:53:47 +0200

    Created main-nav

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 (function () {
+    /** User menu in header */
     const userArr = [];
     const submenu = document.querySelector(".submenu");
 
@@ -9,7 +10,6 @@
     userArr.push(
         document.querySelector(".main-header--user-menu > ul > li:nth-child(2)")
     );
-    console.log(userArr);
 
     userArr[1].addEventListener("click", function () {
         userArr[0].classList.toggle("user-menu-active");
@@ -20,5 +20,33 @@
         } else {
             submenu.style.display = "none";
         }
+    });
+})();
+
+(function () {
+    // search main-nav
+    const navSearchBtn = document.querySelector(".nav-search");
+    const mainNav = document.querySelector(".main-nav");
+    const mainNavSearchForm = document.querySelector(".main-nav--search-form");
+    const mainNavSubmenu = document.querySelector(".submenu");
+
+    navSearchBtn.addEventListener("click", function () {
+        console.log("click");
+        if (mainNav.style.display == "flex") {
+            mainNav.style.display = "none";
+            mainNavSearchForm.style.display = "flex";
+        } else {
+            mainNav.style.display = "flex";
+            mainNavSearchForm.style.display = "none";
+        }
+    });
+})();
+
+(function () {
+    // toggle main-nav
+    const bars = document.querySelector(".submenu .fa-bars");
+    const navSubmenu = document.querySelector("section.nav-submenu");
+    bars.addEventListener("click", function () {
+        navSubmenu.classList.toggle("submenu-active");
     });
 })();
